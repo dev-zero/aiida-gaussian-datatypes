85263603b9e4d6e598a0bc17f01eb7b641c4a885
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-09-03 15:59:44 +0200

    pseudopotential_cli: adapt to new click infra

--- aiida_gaussian_datatypes/console_scripts/pseudopotential_cli.py
+++ aiida_gaussian_datatypes/console_scripts/pseudopotential_cli.py
@@ -1,19 +1,21 @@
 import click
 
-from aiida.cmdline.commands import data_plug
+from aiida.cmdline.commands.cmd_data import verdi_data
+from aiida.cmdline.utils import decorators, echo
 
 
 SUPPORTED_OUTPUT_FORMATS = ['cp2k', ]  # other candidates: 'gaussian', 'gamess', 'nwchem'
 
 
-@data_plug.group('gaussian-basisset')
+@verdi_data.group('gaussian.pseudo')
 def cli():
-    """Command line interface for managing gaussian basissets"""
+    """Manage pseudopotentials for GTO-based codes"""
     pass
 
 
 @cli.command()
-@cli.argument('filename', type=click.Path(exists=True))
+@decorators.with_dbenv()
+@click.argument('filename', type=click.Path(exists=True))
 def upload(filename):
     """
     Upload all pseudopotentials in a file.
@@ -21,29 +23,24 @@ def upload(filename):
     Returns the number of pseudos found and the number of uploaded pseudos.
     """
 
-    from aiida import try_load_dbenv
-    try_load_dbenv()
-
     from aiida_gaussian_datatypes.data.pseudopotential import PseudoPotential as gpp
     n_gpp, n_uploaded = gpp.upload_cp2k_gpp_file(filename)
-    click.echo("Number of pseudos found: {}. Number of new pseudos uploaded: {}".format(n_gpp, n_uploaded))
+    echo.echo("Number of pseudos found: {}. Number of new pseudos uploaded: {}".format(n_gpp, n_uploaded))
 
 
 @cli.command()
-@cli.option('-e', '--element', type=str, default=None, help="Element (e.g. H)")
-@cli.option('ptype', '-t', '--type', type=str, default=None, help="Name or classification (e.g. GTH)")
-@cli.option('-x', '--xcfct', type=str, default=None, help="Associated xc functional (e.g. PBE)")
-@cli.option('-n', '--nval', type=int, default=None, help="Number of valence electrons (e.g. 1)")
-@cli.option('-v', '--version', type=int, default=None, help="specific version")
-@cli.option('-d', '--default', type=bool, default=True,
-            help="show only default pseudos (newest version)", show_defaults=True)
+@decorators.with_dbenv()
+@click.option('-e', '--element', type=str, default=None, help="Element (e.g. H)")
+@click.option('ptype', '-t', '--type', type=str, default=None, help="Name or classification (e.g. GTH)")
+@click.option('-x', '--xcfct', type=str, default=None, help="Associated xc functional (e.g. PBE)")
+@click.option('-n', '--nval', type=int, default=None, help="Number of valence electrons (e.g. 1)")
+@click.option('-v', '--version', type=int, default=None, help="specific version")
+@click.option('-d', '--default', type=bool, default=True,
+              help="show only default pseudos (newest version)", show_default=True)
 def list(element, ptype, xcfct, nval, version, default):
     """
     List AiiDa Gaussian pseudopotentials filtered with optional criteria.
     """
-    from aiida import try_load_dbenv
-    try_load_dbenv()
-
     from aiida.orm import DataFactory
 
     pseudo = DataFactory('gaussian.pseudo')
@@ -57,11 +54,11 @@ def list(element, ptype, xcfct, nval, version, default):
 
     row_format_header = "  {:<10} {:<15} {:<20} {:<10} {:<40} {:<10}"
     row_format = '* {:<10} {:<15} {:<20} {:<10} {:<40} {:<10}'
-    click.echo(row_format_header.format("atom type", "pseudo type", "xc functional", "num. el.", "ID", "version"))
+    echo.echo(row_format_header.format("atom type", "pseudo type", "xc functional", "num. el.", "ID", "version"))
     for pseudo in pseudos:
         pseudo_data = dict(pseudo.iterattrs())
 
-        click.echo(row_format.format(
+        echo.echo(row_format.format(
             pseudo_data['element'],
             pseudo_data['gpp_type'],
             pseudo_data['xc'][0] if pseudo_data['xc'] else '',
@@ -70,7 +67,7 @@ def list(element, ptype, xcfct, nval, version, default):
             pseudo_data['version'][0]))
 
         for i in range(1, len(pseudo_data['id'])):
-            click.echo(row_format.format(
+            echo.echo(row_format.format(
                 '',
                 '(alias)',
                 pseudo_data['xc'][i] if pseudo_data['xc'] else '',
@@ -80,20 +77,19 @@ def list(element, ptype, xcfct, nval, version, default):
 
 
 @cli.command()
-@cli.argument('filename', type=click.Path(exists=False), required=True)
-@cli.option('-e', '--element', type=str, default=None, help="Element (e.g. H)")
-@cli.option('ptype', '-t', '--type', type=str, default=None, help="Name or classification (e.g. GTH)")
-@cli.option('-x', '--xcfct', type=str, default=None, help="Associated xc functional (e.g. PBE)")
-@cli.option('-n', '--nval', type=int, default=None, help="Number of valence electrons (e.g. 1)")
-@cli.option('-v', '--version', type=int, default=None, help="specific version")
-@cli.option('-d', '--default', type=bool, default=True,
-            help="show only default pseudos (newest version)", show_defaults=True)
+@decorators.with_dbenv()
+@click.argument('filename', type=click.Path(exists=False), required=True)
+@click.option('-e', '--element', type=str, default=None, help="Element (e.g. H)")
+@click.option('ptype', '-t', '--type', type=str, default=None, help="Name or classification (e.g. GTH)")
+@click.option('-x', '--xcfct', type=str, default=None, help="Associated xc functional (e.g. PBE)")
+@click.option('-n', '--nval', type=int, default=None, help="Number of valence electrons (e.g. 1)")
+@click.option('-v', '--version', type=int, default=None, help="specific version")
+@click.option('-d', '--default', type=bool, default=True,
+              help="show only default pseudos (newest version)", show_default=True)
 def dump(filename, element, ptype, xcfct, nval, version, default):
     """
     Export AiiDa Gaussian pseudopotentials filtered with optional criteria to file.
     """
-    from aiida import try_load_dbenv
-    try_load_dbenv()
 
     from aiida.orm import DataFactory
 
