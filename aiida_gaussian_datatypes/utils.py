b36043c0ea7bff3328ddb20122042667b9e56d88
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-03-25 17:47:19 +0100

    update copyright year

--- aiida_gaussian_datatypes/utils.py
+++ aiida_gaussian_datatypes/utils.py
@@ -2,7 +2,7 @@
 """
 Gaussian Datatype Plugin helper functions
 
-Copyright (c), 2018 Tiziano Müller
+Copyright (c), Tiziano Müller
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
