a4543a221c42a6d7f05571c95b49eb2a19bb0c97
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-05-06 16:45:59 +0200

    setup: add long description for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,25 @@
 
 import json
 from setuptools import setup, find_packages
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_long_description():
+    readme_md = CURRENT_DIR / "README.md"
+    with open(readme_md, encoding="utf8") as ld_file:
+        return ld_file.read()
+
 
 if __name__ == "__main__":
     # Provide static information in setup.json
     # such that it can be discovered automatically
     with open("setup.json", "r") as info:
         kwargs = json.load(info)
-    setup(packages=find_packages(), **kwargs)
+    setup(
+        packages=find_packages(),
+        long_description=get_long_description(),
+        long_description_content_type="text/markdown",
+        **kwargs
+    )
