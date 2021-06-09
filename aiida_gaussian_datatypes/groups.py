a0d7e3e64c8872bcfbe4de4e7a70d54435ff4b8d
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2021-06-09 11:35:00 +0200

    group: add get_pseudos for pseudo group

--- aiida_gaussian_datatypes/groups.py
+++ aiida_gaussian_datatypes/groups.py
@@ -6,7 +6,11 @@
 Groups for the GTO data classes
 """
 
-from aiida.orm import Group
+from typing import Dict, List, Optional, Sequence
+
+from aiida.orm import Group, QueryBuilder, StructureData
+
+from .pseudopotential.data import Pseudopotential
 
 
 class BasisSetGroup(Group):
@@ -15,3 +19,35 @@ class BasisSetGroup(Group):
 
 class PseudopotentialGroup(Group):
     """Group for Gaussian.Pseudopotential nodes"""
+
+    def get_pseudos(
+        self, elements: Optional[Sequence[str]] = None, structure: Optional[StructureData] = None
+    ) -> Dict[str, List[Pseudopotential]]:
+        """
+        Return a dict of kind names/elements to a list of pseudopotential data nodes
+        for the given list of elements or structure.
+
+        :param elements: list of element symbols.
+        :param structure: the ``StructureData`` node.
+        """
+
+        assert (elements is None) ^ (
+            structure is None
+        ), "Exactly one of the parameters elements and structure must be specified"
+        assert isinstance(elements, Sequence) or isinstance(structure, StructureData)
+
+        if structure:
+            elements = list(structure.get_symbols_set())
+
+        query = (
+            QueryBuilder()
+            .append(self.__class__, filters={"id": self.pk}, tag="group")
+            .append(Pseudopotential, with_group="group", filters={"attributes.element": {"in": elements}})
+        )
+
+        pseudos: Dict[str, List[Pseudopotential]] = {}
+
+        for (pseudo,) in query.iterall():
+            pseudos.setdefault(pseudo.element, []).append(pseudo)
+
+        return pseudos
