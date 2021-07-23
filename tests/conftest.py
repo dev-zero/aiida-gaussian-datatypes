dff3b0ba123802b6906972f45bbe2372c156622f
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2021-07-23 17:13:23 +0200

    tests: fix by getting a fresh DB before each test rather than cleaning up afterwards

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,5 @@ pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]
 
 
 @pytest.fixture(scope="function", autouse=True)
-def clear_database_auto(clear_database):
+def clear_database_auto(clear_database_before_test):
     """Automatically clear database in between tests."""
