7c78e9e9da651c7631491fd3e14635292e549450
addman <pravod@gmail.com>
2021-10-01 13:26:41 +0200

    Add version controling based on commit hashes

--- aiida_gaussian_datatypes/libraries.py
+++ aiida_gaussian_datatypes/libraries.py
@@ -9,11 +9,16 @@ import re
 import git
 import tempfile
 import pathlib
+import pydriller
 from aiida_gaussian_datatypes import utils
 from typing import Dict, Generic, List, Optional, Sequence, Type, TypeVar
 from icecream import ic
 from .basisset.data import BasisSet
-from .pseudopotential.data import Pseudopotential
+from .pseudopotential.data import Pseudopotential, ECPPseudopotential
+
+from aiida.common.exceptions import (
+    NotExistent,
+)
 
 class LibraryBookKeeper:
 
@@ -57,7 +62,7 @@ class QmcpackLibrary(_ExternalLibrary):
     def fetch(cls):
 
         elements = {}
-        def add_data(p, elements = elements):
+        def add_data(p, tempdir, elements = elements):
             element = str(p.parent.parent.name)
             if element not in utils.SYM2NUM: # Check if element is valid
                 return
@@ -73,6 +78,22 @@ class QmcpackLibrary(_ExternalLibrary):
                 pseudo, = Pseudopotential.from_gamess(fhandle,
                                                       duplicate_handling = "force-ignore",
                                                       attrs = {"name" : typ })
+
+            commithash = ""
+            for commit in pydriller.Repository(str(tempdir), filepath=str(p)).traverse_commits():
+                commithash = commit.hash
+            if commithash == "": return
+            pseudo.extras["commithash"] = commithash
+
+            try:
+                latest = ECPPseudopotential.get(pseudo.element,
+                                                pseudo.name)
+                pseudo.version = latest.version
+                if latest.extras["commithash"] != commithash:
+                    pseudo.version += 1
+            except NotExistent:
+                pass
+
             tags.append(f"q{pseudo.n_el_tot}")
             tags.append(f"c{pseudo.core_electrons}")
             pseudo.tags.extend(tags)
@@ -94,8 +115,25 @@ class QmcpackLibrary(_ExternalLibrary):
                                                           "tags": tags})
                         if len(b) == 0: continue
                         b, = b
+
+                        commithash = ""
+                        for commit in pydriller.Repository(str(tempdir), filepath=str(r)).traverse_commits():
+                            commithash = commit.hash
+                        if commithash == "": return
+                        b.extras["commithash"] = commithash
+
+                        try:
+                            latest = BasisSet.get(b.element,
+                                                  b.name)
+                            b.version = latest.version
+                            if latest.extras["commithash"] != commithash:
+                                b.version += 1
+                        except NotExistent:
+                            pass
+
                     basis.append({"path": r,
                                   "obj": b})
+
 
             if element not in elements:
                 elements[element] = {"path": element_path,
@@ -112,7 +150,7 @@ class QmcpackLibrary(_ExternalLibrary):
 
         for p in (tempdir/"recipes").glob("**/*"):
             if re.match("[A-z]{1,2}\.ccECP\.gamess", p.name):
-                add_data(p)
+                add_data(p, tempdir)
 
         return elements
 
